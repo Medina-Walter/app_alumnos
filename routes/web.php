7e6b1f440bb6cb8ecf57cecee9533a032ff49084
J0S3S0S4 <josesosa602503@gmail.com>
2025-06-28 17:59:15 -0300

    Avances: migraciones, modelos y nuevas carpetas

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,17 @@ use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use App\Livewire\Clientes;
 use App\Livewire\Producto\FormularioProductos;
-
-
-
-
-
-
+use App\Http\Controllers\TallerController;
 
 Route::get('/login', Login::class)->name('login');
 Route::get('/', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
 
-
-// Ruta protegida
+// Rutas protegidas por autenticación
 Route::middleware(['auth'])->group(function () {
     Route::get('/clientes', Clientes::class)->name('clientes');
     Route::get('/productos', FormularioProductos::class)->name('productos');
+
+    // Nueva ruta para ver los talleres
+    Route::get('/talleres', [TallerController::class, 'index'])->name('talleres.index');
 });
